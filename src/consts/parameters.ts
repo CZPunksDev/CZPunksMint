06d03207b33f20e99d2396a2b0321b229bc97a1a
CZPunksDev <152563731+CZPunksDev@users.noreply.github.com>
2023-12-03 01:17:54 -0500

    Update parameters.ts

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -12,7 +12,7 @@ export const chainConst = "ethereum";
 // Please ensure that you define the correct domain for your API key from the API settings page.
 // You can get one for free at https://thirdweb.com/create-api-key
 // Learn more here: https://blog.thirdweb.com/changelog/api-keys-to-access-thirdweb-infra
-export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "";
+export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "008ad564f0bf97ff13bf3e2a9da2e0bd";
 
 // Configure the primary color for buttons and other UI elements
 export const primaryColorConst = "blue";
