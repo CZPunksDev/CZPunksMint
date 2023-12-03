54428fb06963ac5191d2fe41e244f73de5bcdd90
CZPunksDev <152563731+CZPunksDev@users.noreply.github.com>
2023-12-03 01:15:02 -0500

    Update parameters.ts
    Updated params

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,11 +2,11 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const contractConst = "0x027DE225D5728c5beae53F5c36aD5e21Be550Ea3";
 
 // The name of the chain your contract is deployed to.
 // Refer to README.md on how to specify the chain name.
-export const chainConst = "mumbai";
+export const chainConst = "ethereum";
 
 // It is IMPORTANT to provide your own API key to use the thirdweb SDK and infrastructure.
 // Please ensure that you define the correct domain for your API key from the API settings page.
